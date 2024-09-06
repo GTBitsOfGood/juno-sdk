8a44e84b86eb8e14954499e17874350f189f47b2
Syaam Khandaker <syaamkhandaker@gmail.com>
2024-09-06 14:55:03 -0400

    added input checker type and getProject method

--- src/lib/project.ts
+++ src/lib/project.ts
@@ -1,19 +1,36 @@
+import { ProjectApi } from "../internal/api";
+
+type projectInputType = {
+  name: string;
+  id?: never;
+} | {
+  id: string;
+  name?: never;
+};
+
 type ProjectAPI = {
   createProject: () => void;
   // Shou;d be by ID or Name
-  getProject: () => void;
+  getProject: (input: projectInputType) => void;
   // Shou;d be by ID or Name
-  linkProjectToUser: () => void;
+  linkProjectToUser: (input: projectInputType) => void;
 };
+
+const projectApi = new ProjectApi();
 
 export const projectAPI: ProjectAPI = {
   createProject: function (): void {
     throw new Error('Function not implemented.');
   },
-  getProject: function (): void {
-    throw new Error('Function not implemented.');
+
+  getProject: function (input: projectInputType): Promise<any> {
+    if (!input.id) {
+      return projectApi.projectControllerGetProjectByName(input.name)
+    } 
+      return projectApi.projectControllerGetProjectById(input.id)
   },
-  linkProjectToUser: function (): void {
+
+  linkProjectToUser: function (input: projectInputType): void {
     throw new Error('Function not implemented.');
   },
 };
